2ccfe4786627ab15ba99caaac88451d5585f79b6
abul05 <ah0907@edu.turku.fi>
2023-03-23 08:24:10 +0200

    Create huihai.py

---
+++ huihai.py
@@ -0,0 +1,9 @@
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("HuiHai")
+    elif i % 3 == 0:
+        print("Hui")
+    elif i % 5 == 0:
+        print("Hai")
+    else:
+        print(i)
